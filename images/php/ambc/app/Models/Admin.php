d7223dcfdc39051d13a7f386c6bc5994f845f9ba
yinghua <yinghua@repromarketing.com>
2020-11-04 12:22:00 +0800

    chore: modify environments and create model for admin

---
+++ images/php/ambc/app/Models/Admin.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Admin extends Model
+{
+    use SoftDeletes;
+
+    protected $table = 'admins';
+
+    protected $fillable = [
+        'first_name',
+        'last_name '
+    ];
+}
