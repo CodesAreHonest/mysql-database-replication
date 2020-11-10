998b3a9bbb74b921041ce27ac224f8684e022bd6
yinghua <yinghua@repromarketing.com>
2020-11-10 12:24:16 +0800

    feat: database replication setup completed

--- commands/db_replication/slave.sh
+++ commands/db_replication/slave.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
+if [[ $# -eq 0 ]] ; then
+    echo 'Please provide mysql binary file name.'
+    exit 1
+fi
 
 ## 4. Set Replication
 
 for N in 1 2
   do docker exec -it ambc_slave_db_$N mysql -uroot -psecurerootpassword \
-    -e "CHANGE MASTER TO MASTER_HOST='ambc_slave_db', MASTER_USER='repl', \
-      MASTER_PASSWORD='slavepass', MASTER_LOG_FILE='$0';"
+    -e "CHANGE MASTER TO MASTER_HOST='ambc_master_db', MASTER_USER='repl', \
+      MASTER_PASSWORD='slavepass', MASTER_LOG_FILE='binlog.$1';"
 
   docker exec -it ambc_slave_db_$N mysql -uroot -psecurerootpassword -e "START SLAVE;"
 done
@@ -15,6 +19,5 @@ done
 ## 5. Show Replication Status
 
 for N in 1 2
-  do docker exec -it ambc_slave_db_$N mysql -uroot -psecurerootpassword -e "SHOW SLAVE STATUS\G"
   do docker exec -it ambc_slave_db_$N mysql -uroot -psecurerootpassword -e "SHOW SLAVE STATUS\G"
 done 
