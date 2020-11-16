8e758f6d548dd18491e3b8526f075f3e19de49f4
yinghua <yinghua@repromarketing.com>
2020-11-16 12:59:32 +0800

    feat: docker monitoring configuration

--- commands/ambc/up.sh
+++ commands/ambc/up.sh
@@ -1,12 +1,3 @@
 #!/usr/bin/env bash
 
 docker-compose up -d --force-recreate
-
-# Start Node Exporter to collect OS Metrics and Network Information
-docker run --name="ambc_node_exporter" \
-  -d \
-  --net="host" \
-  --pid="host" \
-  -v "/:/host:ro,rslave" \
-  quay.io/prometheus/node-exporter \
-  --path.rootfs=/host
